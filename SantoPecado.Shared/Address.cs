8a6a334a2a2e8c76a1f5b55696fa6f586a799f9b
Kike <kikemjunior@gmail.com>
2022-01-24 12:25:59 +0100

    Checkout validación

--- SantoPecado.Shared/Address.cs
+++ SantoPecado.Shared/Address.cs
@@ -6,16 +6,22 @@ namespace SantoPecado
     {
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "Debes introducir un nombre."), MaxLength(100)]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "Debes introducir una dirección."), MaxLength(100)]
         public string Line1 { get; set; }
 
+        [MaxLength(100)]
         public string Line2 { get; set; }
 
+        [Required(ErrorMessage = "Debes introducir una ciudad."), MaxLength(50)]
         public string City { get; set; }
 
+        [Required(ErrorMessage = "Debes introducir una provincia."), MaxLength(20)]
         public string Region { get; set; }
 
+        [Required(ErrorMessage = "Debes introducir un código postal."), MaxLength(20)]
         public string PostalCode { get; set; }
     }
 }
