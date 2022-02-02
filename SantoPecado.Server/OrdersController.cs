0d4907bc46a6f87dc7f858e6c082998145fdff88
emp718 <emp718@inlumine.ual.es>
2022-02-02 22:03:00 +0100

    Completar el envío y visualización de notificaciones. Así como el manejo de clics sobre ellas.

--- SantoPecado.Server/OrdersController.cs
+++ SantoPecado.Server/OrdersController.cs
@@ -106,10 +106,28 @@ namespace SantoPecado.Server
             await SendNotificationAsync(order, subscription, "Su pedido ha sido entregado. ¡Disfrútelo!");
         }
 
-        private static Task SendNotificationAsync(Order order, NotificationSubscription subscription, string message)
+        private static async Task SendNotificationAsync(Order order, NotificationSubscription subscription, string message)
         {
-            // This will be implemented later
-            return Task.CompletedTask;
+            // For a real application, generate your own
+            var publicKey = "BLC8GOevpcpjQiLkO7JmVClQjycvTCYWm6Cq_a7wJZlstGTVZvwGFFHMYfXt6Njyvgx_GlXJeo5cSiZ1y4JOx1o";
+            var privateKey = "OrubzSz3yWACscZXjFQrrtDwCKg-TGFuWhluQ2wLXDo";
+
+            var pushSubscription = new PushSubscription(subscription.Url, subscription.P256dh, subscription.Auth);
+            var vapidDetails = new VapidDetails("mailto:<someone@example.com>", publicKey, privateKey);
+            var webPushClient = new WebPushClient();
+            try
+            {
+                var payload = JsonSerializer.Serialize(new
+                {
+                    message,
+                    url = $"myorders/{order.OrderId}",
+                });
+                await webPushClient.SendNotificationAsync(pushSubscription, payload, vapidDetails);
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine("Error al enviar notificación push: " + ex.Message);
+            }
         }
     }
 }
